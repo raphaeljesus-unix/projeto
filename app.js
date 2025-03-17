9b1f6532aa9d1351e5b7afdb84d75e33d0d83036
raphaeljesus-unix <gzuix.021@gmail.com>
2025-03-17 15:15:53 -0300

    Revert "alterando limite para 100"
    This reverts commit 503052ce3fd2f5c0e1112849c66c67c926ae0d06.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let listaDeNumerosSorteados = [];
+<<<<<<< HEAD
 let numeroLimite = 40;
+=======
+let numeroLimite = 10;
+>>>>>>> parent of 503052c (alterando limite para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +15,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
